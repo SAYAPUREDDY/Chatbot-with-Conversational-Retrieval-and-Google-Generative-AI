8003838cf892bb9aa800bbc5b0437aebb3af5a60
Sayapureddy <tejasurya0953@gmail.com>
2024-09-22 16:57:25 +0200

    updated

--- chatbot.py
+++ chatbot.py
@@ -13,11 +13,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
-print("successfully configured")
+#print("successfully configured")
 
 llm=model = ChatGoogleGenerativeAI(model="gemini-1.5-flash",temperature=0.7)
-print("llm initialized")
-
+#print("llm initialized")
+# to remember the chat we will import the memeory from langchain
 memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
 embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
 vector_db = Chroma(embedding_function=embeddings, collection_name="my_collection",persist_directory="./my_chroma_db")
